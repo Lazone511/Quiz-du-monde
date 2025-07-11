201721ce598b77bf4c649ab191c512fbe320b294
Arthur <arthuryazjyan@gmail.com>
2025-07-11 21:56:29 +0200

    V1
    Quiz monde

---
+++ dist/messages_fr.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$.quiz('localization', {
+    start: 'Démarrer',
+    prev: 'Précédent',
+    next: 'Suivant',
+    results: 'Aller aux résultats',
+    restart: 'Retour',
+    nextQuiz: 'Aller au quiz suivant',
+    error: 'Erreur',
+    errmsg: [
+        'Veuillez choisir une réponse.',
+        'Certaines questions sont restées sans réponse. Veuillez revenir en haut pour répondre à toutes les questions.'
+    ]
+});
